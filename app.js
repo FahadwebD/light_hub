9d80018701c9f0a4a19a7eed1fc374c1fb21cdcb
FahadwebD <fahadchowdhury66779@gmail.com>
2022-05-19 23:27:20 +0600

    finished task 1

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('connected')
+
+var type='off'
+function lightOn (){
+    if (type == 'on'){
+        type = 'off';
+    }
+    else{
+        type ='on';
+    }
+    document.getElementById('img').src='image/'+type+'.png';
+    document.getElementById('img').value=type;
+}
